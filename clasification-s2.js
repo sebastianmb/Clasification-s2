b7af3cad49218d7d925205406d372297c9e90ede
sebastianmb <sebastianmb88@gmail.com>
2022-11-26 23:23:25 -0500

    Configuración inicial UI

--- clasification-s2.js
+++ clasification-s2.js
@@ -1,38 +1,108 @@
-var startDate = '2022-08-18';
-var endDate = '2022-10-09';
-var geometry = ee.Geometry.Point([286.952564717386,9.055927787338167 ]||[286.852564717386,7.055927787338167 ]);
+var filtered = table;
+var geometry = filtered.geometry();
+Map.centerObject(geometry, 10);
 
-var s2 = ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-             .filterDate(startDate, endDate)
-             .filterBounds(geometry)
-             .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 50));
-             
-             
-var s2Image = ee.Image(s2.first());
-var s2VisParams = {bands: ['B4', 'B3', 'B2'], min: 0, max: 3000};
-Map.addLayer(s2Image, s2VisParams, 'Sentinel-2 Image');
-Map.centerObject(geometry, 8);
-
-//********Find the Matching Dynamic World Image
-var imageId = s2Image.get('system:index');
-print(imageId);
+var startDate = '2022-07-01';
+var endDate = '2022-10-10';
 
 var dw = ee.ImageCollection('GOOGLE/DYNAMICWORLD/V1')
-             .filter(ee.Filter.eq('system:index', imageId));
-var dwImage = ee.Image(dw.first());
-print(dwImage);
+  .filterDate(startDate, endDate)
+  .filterBounds(geometry);
 
-//********************* Visualize the Classified Image
-var classification = dwImage.select('label');
+// Create a Mode Composite.
+var classification = dw.select('label');
+var dwComposite = classification.reduce(ee.Reducer.mode());
+
+// Extract the Built Area class.
+var builtArea = dwComposite.eq(6);
+
 var dwVisParams = {
   min: 0,
   max: 8,
-  palette: [
-    '#419BDF', '#397D49', '#88B053', '#7A87C6', '#E49635', '#DFC35A',
-    '#C4281B', '#A59B8F', '#B39FE1'
-  ]
+  palette: ['#419BDF', '#397D49', '#88B053', '#7A87C6',
+    '#E49635', '#DFC35A', '#C4281B', '#A59B8F', '#B39FE1']
 };
 
-Map.addLayer(classification.clip(table), dwVisParams, 'Classified Image');
+// Clip the composite and add it to the Map.
+Map.addLayer(dwComposite.clip(geometry), dwVisParams, 'Classified Composite');
+//Map.addLayer(builtArea.clip(geometry), {}, 'Built Areas');
+Map.addLayer(presioneslb);
 
-Map.addLayer(table);
+// Rename the band names.
+var dwComposite = dwComposite.rename(['classification']);
+var builtArea = builtArea.rename(['built_area']);
+
+// Calculate Pixel Counts.
+/*
+// Count all pixels.
+var statsTotal = builtArea.reduceRegion({
+    reducer: ee.Reducer.count(),
+    geometry: geometry,
+    scale: 10,
+    maxPixels: 1e10
+    }); 
+var totalPixels = statsTotal.get('built_area');
+
+// Mask 0 pixel values and count remaining pixels.
+var builtAreaMasked = builtArea.selfMask();
+
+var statsMasked = builtAreaMasked.reduceRegion({
+    reducer: ee.Reducer.count(),
+    geometry: geometry,
+    scale: 10,
+    maxPixels: 1e10
+    }); 
+var builtAreaPixels = statsMasked.get('built_area');
+print('Built area pixel count', builtAreaPixels);
+var fraction = (ee.Number(builtAreaPixels).divide(totalPixels))
+  .multiply(100);
+print('Percentage built area', fraction.format('%.2f'));
+
+// Calculating Pixel Counts for all classes.
+var pixelCountStats = dwComposite.reduceRegion({
+    reducer: ee.Reducer.frequencyHistogram().unweighted(),
+    geometry: geometry,
+    scale: 10,
+    maxPixels: 1e10
+    }); 
+ 
+var pixelCounts = ee.Dictionary(pixelCountStats.get('classification'));
+print('Pixel counts per class', pixelCounts);
+
+// Format the results to make it more readable.
+var classLabels = ee.List([
+    'water', 'trees', 'grass', 'flooded_vegetation', 'crops',
+    'shrub_and_scrub', 'built', 'bare', 'snow_and_ice'
+    ]);
+
+// Rename keys with class names.
+var pixelCountsFormatted = pixelCounts.rename(
+  pixelCounts.keys(), classLabels);
+print('Pixel counts per class (formatted)', pixelCountsFormatted);
+
+// Create a Feature Collection.
+var exportFc = ee.FeatureCollection(
+  ee.Feature(null, pixelCountsFormatted));
+
+// Export the results as a CSV file.
+Export.table.toDrive({
+  collection: exportFc,
+  description: 'pixel_counts_export',
+  folder: 'earthengine',
+  fileNamePrefix: 'pixel_counts',
+  fileFormat: 'CSV',
+});
+*/
+
+/*
+ * Map panel configuration
+ */
+
+// Now let's do some overall layout.
+// Create a map panel.
+
+var mapPanel = ui.Map();
+
+// Take all tools off the map except the zoom and mapTypeControl tools.
+mapPanel.setControlVisibility(
+    {all: false, zoomControl: true, mapTypeControl: true});
